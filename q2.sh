52b6b568af0486237c7199ab0b59a37851bff8d0
manojugowda <manoj.u46@gmail.com>
2021-12-15 11:52:42 +0000

    file added

---
+++ q2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "enter the name of the employee"
+read name
+echo "enter the designation"
+read designation
+echo "enter the age of the employee"
+read age
+echo "enter the company name"
+read company
+echo "the name of the employee is $name"
+echo "the designation of the employee is $designation"
+echo "the age of the employee is $age"
+echo "the company name is $company"
